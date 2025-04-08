5566b62d1a67627bf17196fb5d1040d4f4adc84e
EP0923 <146797482+ElmerPonce07@users.noreply.github.com>
2025-04-08 00:57:09 -0400

    delete

--- src/config/firebase.jsx
+++ src/config/firebase.jsx
@@ -3,15 +3,7 @@ import { getAnalytics } from "firebase/analytics";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyARgB-MElBc58dljhTIiIbe4hNBDUgNgTw",
-  authDomain: "fir-learn-1f798.firebaseapp.com",
-  projectId: "fir-learn-1f798",
-  storageBucket: "fir-learn-1f798.firebasestorage.app",
-  messagingSenderId: "222871204712",
-  appId: "1:222871204712:web:2a6adf0d6b007d52571cdd",
-  measurementId: "G-C94DM917C2",
-};
+
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
